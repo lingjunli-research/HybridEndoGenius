b22f8fb519aaebd2b7194f19bae79cdeef54ec77
jqin53 <jqin53@ap2002.chtc.wisc.edu>
2026-02-21 12:42:41 -0600

    Confirm consistent variables for Casanovo

--- casanovo.sh
+++ casanovo.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+set -e
 
+MGF_FILE="$1"
+YAML_FILE="$2"
+CKPT_FILE="$3"
+
+echo "Running Casanovo"
+echo "MGF: $MGF_FILE"
+echo "YAML: $YAML_FILE"
+echo "Checkpoint: $CKPT_FILE"
 # Run Casanovo inside the container
 singularity exec docker://mriffle/casanovo:4.2.1 \
   casanovo sequence \
-    -o ${1%.mgf}.mztab \
-    -m casanovo_v4_2_0.ckpt \
-    -c casanovo.yaml \
-    $1
+    -o ${MGF_FILE%.mgf}.mztab \
+    -m "$CKPT_FILE" \
+    -c "$YAML_FILE" \
+    "$MGF_FILE"
